eafa71a94270c3d72c2bf2acba151fc301bbe275
viktarian dell ubuntu <viktar.akulenka@yandex.ru>
2015-11-23 23:28:22 +0200

    banner

--- quick/lampp/testemail/copy/index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-//phpinfo();
-//print_r(apache_get_modules());
-?>
-<a href="http://testemail.mvolna.by/admin" target="blank">admin</a> (login: admin, password: admin)<br>
-<a href="http://testemail.mvolna.by/user" target="blank">user</a><br>
--- quick/lampp/testemail/copy/user/unsubscribe/script.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../../config.php');
-include('../../library/ClassMySQL.php');
-$mysql=new ClassMySQL();
-$mysql->update($table='subscription',$name_column='send',$info='0',$id=$_POST['id']);
-
-echo'<p>Вы успешно отписаны.</p>';
-?>
